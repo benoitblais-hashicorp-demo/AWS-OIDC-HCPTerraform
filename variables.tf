33bf4622faeb01b928e4ca5fd3431d227eb41623
Benoit Blais <benoit.blais@hashicorp.com>
2025-11-13 14:53:13 -0500

    initial release

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "tfc_organization_name" {
+  description = "(Required) The name of the HCP Terraform organization."
+  type        = string
+  default     = "benoitblais-hashicorp"
+}
+
+variable "tfc_project_name" {
+  description = "(Required) The name of the HCP Terraform project."
+  type        = string
+  default     ="*"
+}
+
+variable "tfc_workspace_name" {
+  description = "(Required) The name of the HCP Terraform workspace."
+  type        = string
+  default     ="*"
+}
+
+variable "tfc_hostname" {
+  description = "(Optional) The address of the HCP Terraform instance."
+  type        = string
+  default     = "app.terraform.io"
+}
